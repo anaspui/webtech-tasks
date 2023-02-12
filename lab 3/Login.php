1c85da9c64e5bd79516837e4b213ab65d1729749
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-02-12 20:49:18 +0600

    12/02/23

--- lab 3/Login.php
+++ lab 3/Login.php
@@ -1,19 +1,41 @@
-<?php
-// var_dump($_REQUEST);
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<html>
 
-        $username = $_REQUEST["username"];
-        $password = $_REQUEST["password"];
+<head>
+    <title>Login</title>
+</head>
 
-        if (empty($username)) {
-                echo "Please fillup Username.<br>";
-        }
-        if (empty($password)) {
-                echo "Please fillup Password.<br>";
-        } else {
-                echo "<h1>Login successful</h1>";
+<body>
+    <div align="center">
+        <?php
+                function sanitize($data)
+                {
+                        $data = trim($data);
+                        $data = stripslashes($data);
+                        $data = htmlspecialchars($data);
+                        return $data;
+                }
+                // var_dump($_REQUEST);
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $flag = true;
+                        $username = sanitize($_POST["username"]);
+                        $password = sanitize($_POST["password"]);
 
-        }
+                        if (empty($username)) {
+                                $flag = false;
+                                echo "Please fillup Username.<br>";
+                        }
+                        if (empty($password)) {
+                                $flag = false;
+                                echo "Please fillup Password.<br>";
+                        }
+                        if ($flag === true) {
+                                echo "<h1>Login successful</h1>";
 
-}
-?>
+                        }
+
+                }
+                ?>
+    </div>
+</body>
+
+</html>
--- lab 3/Registration.php
+++ lab 3/Registration.php
@@ -1,222 +1,223 @@
-<?php
-function sanitize($data)
-{
-    $data = htmlspecialchars($data);
-    return $data;
-}
-var_dump($_REQUEST);
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fname = $_REQUEST["fname"];
-    $lname = $_REQUEST["lname"];
-    $faname = $_REQUEST["faname"];
-    $moname = $_REQUEST["moname"];
-    $bgrp = $_REQUEST["bgrp"];
-    $gender = $_REQUEST["gender"];
-    $dob = $_REQUEST["date"];
-    $email = $_REQUEST["email"];
-    $phn = $_REQUEST["phn"];
-    $web = $_REQUEST["web"];
-    $pad = $_REQUEST["pad"];
-    $username = $_REQUEST["username"];
-    $password = $_REQUEST["password"];
-
-    if (empty($fname)) {
-        echo "Please fillup First Name.<br>";
-    }
-    if (empty($lname)) {
-        echo "Please fillup Last Name.<br>";
-    }
-    if (empty($faname)) {
-        echo "Please fillup Fathers Name.<br>";
-    }
-    if (empty($moname)) {
-        echo "Please fillup Mothers Name.<br>";
-    }
-    if (empty($bgrp)) {
-        echo "Please fillup Blood Group.<br>";
-    }
-    if (empty($email)) {
-        echo "Please fillup Email.<br>";
-    }
-    if (empty($phn)) {
-        echo "Please fillup Phone/Mobile.<br>";
-    }
-    if (empty($web)) {
-        echo "Please fillup website.<br>";
-    }
-    if (empty($pad)) {
-        echo "Please fillup present address.<br>";
-    }
-    if (empty($username)) {
-        echo "Please fillup Username.<br>";
-    }
-    if (empty($password)) {
-        echo "Please fillup Password.<br>";
-    } else {
-        echo "  ";
-    }
-}
-?>
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>
-        Registration Page
-    </title>
-    <style>
-        .center {
-            margin-left: auto;
-            margin-right: auto;
-
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        button {
-            text-align: center;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
 </head>
 
 <body>
-    <table class="center">
+    <div align="center">
         <h1>Registration</h1>
-        <tr>
-            <td>
-                <fieldset>
-                    <Legend><b>General Information: </b></Legend>
-                    <table>
-                        <tr>
-                            <th><label for="fname">First Name:</label></th>
-                            <td><label for="">
-                                    <?php echo $fname ?>
-                                </label></Input></td>
-                        </tr>
-                        <tr>
-                            <th><label for="lname">Last Name:</label></th>
-                            <td><label for="">
-                                    <?php echo $lname ?>
-                                </label></Input></td>
-                        </tr>
-                        <tr>
-                            <th><label for="faname">Father's Name:</label></th>
-                            <td><label for="">
-                                    <?php echo $faname ?>
-                                </label></Input></td>
-                        </tr>
-                        <tr>
-                            <th><label for="moname">Mother's Name:</label></th>
-                            <td><label for="">
-                                    <?php echo $moname ?>
-                                </label></Input></td>
+        <?php
+        function sanitize($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        //var_dump($_REQUEST);
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $fname = $_POST["fname"];
+            $lname = $_POST["lname"];
+            $faname = $_POST["faname"];
+            $moname = $_POST["moname"];
+            $bgrp = $_POST["bgrp"];
+            $gender = $_POST["gender"];
+            $dob = $_POST["date"];
+            $email = $_POST["email"];
+            $phn = $_POST["phn"];
+            $web = $_POST["web"];
+            $pad = $_POST["pad"];
+            $username = sanitize($_POST["username"]);
+            $password = sanitize($_POST["password"]);
+            $flag = true;
+            if (empty($fname)) {
+                $flag = false;
+                echo "Please fillup First Name.<br>";
+            }
+            if (empty($lname)) {
+                $flag = false;
+                echo "Please fillup Last Name.<br>";
+            }
+            if (empty($faname)) {
+                $flag = false;
+                echo "Please fillup Fathers Name.<br>";
+            }
+            if (empty($moname)) {
+                $flag = false;
+                echo "Please fillup Mothers Name.<br>";
+            }
+            if (empty($bgrp)) {
+                $flag = false;
+                echo "Please fillup Blood Group.<br>";
+            }
+            if (empty($email)) {
+                $flag = false;
+                echo "Please fillup Email.<br>";
+            }
+            if (empty($phn)) {
+                $flag = false;
+                echo "Please fillup Phone/Mobile.<br>";
+            }
+            if (empty($web)) {
+                $flag = false;
+                echo "Please fillup website.<br>";
+            }
+            if (empty($pad)) {
+                $flag = false;
+                echo "Please fillup present address.<br>";
+            }
+            if (empty($username)) {
+                $flag = false;
+                echo "Please fillup Username.<br>";
+            }
+            if (empty($password)) {
+                $flag = false;
+                echo "Please fillup Password.<br>";
+            } else {
+                $flag = true;
+                echo "<p><u>Registration Successful.</u></p><br>";
+            }
+        }
 
-                        </tr>
-                        <tr>
-                            <th><label for="male">Gender: </label> </th>
+        ?>
+        <table class="center">
+            <tr>
+                <td>
+                    <fieldset>
+                        <Legend><b>General Information: </b></Legend>
+                        <table>
+                            <tr>
+                                <th><label for="fname">First Name:</label></th>
+                                <td><label for="">
+                                        <?php echo $fname ?>
+                                    </label></Input></td>
+                            </tr>
+                            <tr>
+                                <th><label for="lname">Last Name:</label></th>
+                                <td><label for="">
+                                        <?php echo $lname ?>
+                                    </label></Input></td>
+                            </tr>
+                            <tr>
+                                <th><label for="faname">Father's Name:</label></th>
+                                <td><label for="">
+                                        <?php echo $faname ?>
+                                    </label></Input></td>
+                            </tr>
+                            <tr>
+                                <th><label for="moname">Mother's Name:</label></th>
+                                <td><label for="">
+                                        <?php echo $moname ?>
+                                    </label></Input></td>
 
-                            <td><label for="">
-                                    <?php echo $gender ?>
-                                </label></Input></td>
+                            </tr>
+                            <tr>
+                                <th><label for="male">Gender: </label> </th>
+
+                                <td><label for="">
+                                        <?php echo $gender ?>
+                                    </label></Input></td>
 
 
-                        </tr>
-                        <tr>
-                            <th><label for="date">Date of Birth</label></th>
-                            <td><label for="">
-                                    <?php echo $dob ?>
-                                </label></Input></td>
-                        </tr>
-                        <tr>
-                            <th><label for="bgrp">Blood Group:</label> </th>
-                            <td><label for="">
-                                    <?php echo $bgrp ?>
-                                </label></td>
-                        </tr>
-                    </table>
-                </fieldset>
-            </td>
+                            </tr>
+                            <tr>
+                                <th><label for="date">Date of Birth</label></th>
+                                <td><label for="">
+                                        <?php echo $dob ?>
+                                    </label></Input></td>
+                            </tr>
+                            <tr>
+                                <th><label for="bgrp">Blood Group:</label> </th>
+                                <td><label for="">
+                                        <?php echo $bgrp ?>
+                                    </label></td>
+                            </tr>
+                        </table>
+                    </fieldset>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <fieldset>
+                        <legend><b>Contact Information: </b></legend>
+                        <table>
+                            <tr>
+                                <th><label for="email">Email: </label></th>
+                                <td>
+                                <td><label for="">
+                                        <?php echo $email ?>
+                                    </label></Input></td>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="phn">Phone/Mobile: </label></th>
+                <td>
+                <td><label for="">
+                        <?php echo $phn ?>
+                    </label></Input></td>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="web">Website: </label></th>
+                <td>
+                <td><label for="">
+                        <?php echo $web ?>
+                    </label></Input></td>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="pad">Present Address: </label></th>
+                <td>
+                <td><label for="">
+                        <?php echo $pad ?>
+                    </label></Input></td>
+                </td>
+            </tr>
+
+        </table>
+        </fieldset>
+        </td>
         </tr>
         <tr>
+        <tr>
             <td>
                 <fieldset>
-                    <legend><b>Contact Information: </b></legend>
+                    <legend><b>Account Information: </b></legend>
                     <table>
                         <tr>
-                            <th><label for="email">Email: </label></th>
+                            <th><label for="Username">Username: </label></th>
                             <td>
                             <td><label for="">
-                                    <?php echo $email ?>
-                                </label></Input></td>
+                                    <?php echo $username ?>
+                                </label></Input></td></Input>
             </td>
         </tr>
         <tr>
-            <th><label for="phn">Phone/Mobile: </label></th>
+            <th><label for="password">Password: </label></th>
             <td>
             <td><label for="">
-                    <?php echo $phn ?>
-                </label></Input></td>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="web">Website: </label></th>
-            <td>
-            <td><label for="">
-                    <?php echo $web ?>
-                </label></Input></td>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="pad">Present Address: </label></th>
-            <td>
-            <td><label for="">
-                    <?php echo $pad ?>
+                    <?php echo "********" ?>
                 </label></Input></td>
             </td>
         </tr>
 
-    </table>
-    </fieldset>
-    </td>
-    </tr>
-    <tr>
-    <tr>
-        <td>
-            <fieldset>
-                <legend><b>Account Information: </b></legend>
-                <table>
-                    <tr>
-                        <th><label for="Username">Username: </label></th>
-                        <td>
-                        <td><label for="">
-                                <?php echo $username ?>
-                            </label></Input></td></Input>
+        </table>
+        </fieldset>
         </td>
-    </tr>
-    <tr>
-        <th><label for="password">Password: </label></th>
-        <td>
-        <td><label for="">
-                <?php echo "********" ?>
-            </label></Input></td>
-        </td>
-    </tr>
-
-    </table>
-    </fieldset>
-    </td>
-    </tr>
-    </tr>
-    <tr>
-        <td>
-            <a href="Login.html">Login</a>
-        </td>
-    </tr>
-    </table>
+        </tr>
+        </tr>
+        <tr>
+            <td>
+                <a href="Login.html">Login</a>
+            </td>
+        </tr>
+        </table>
+    </div>
 </body>
 
 </html>
