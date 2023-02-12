bce925cc585525a64a0d0a2a606b4c434b69a02e
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-02-12 13:51:19 +0600

    02/12/23

---
+++ lab 3/Login.php
@@ -0,0 +1,20 @@
+<?php
+// var_dump($_REQUEST);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $username = $_REQUEST["username"];
+        $password = $_REQUEST["password"];
+
+        if (empty($username)) {
+                echo "Please fillup Username.<br>";
+        }
+        if (empty($password)) {
+                echo "Please fillup Password.<br>";
+        }
+        else{
+                echo "<h1>Login successful</h1>";
+                
+        }
+
+}
+?>
