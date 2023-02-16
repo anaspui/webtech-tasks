7aac2deee076c9f554f71a4de01cfab385127dfb
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-02-16 20:24:06 +0600

    16/02/23

--- lab 4/Cookie1.php
+++ lab 4/Cookie1.php
@@ -1,4 +1,26 @@
 <?php
+//Time zone of Bangladesh
+date_default_timezone_set("Asia/Dacca");
+
+//Checks cookie settings
+if (!isset($_COOKIE['lastVisited'])) {
+    $lastVisited = "Your're Visiting this site for the first time";
+    echo $lastVisited;
+    setcookie('lastVisited', date('d-m-Y', time()));
+} else if (isset($_COOKIE['lastVisited'])) {
+    $lastVisited = $_COOKIE['lastVisited'];
+    setcookie('lastVisited', date('d-m-Y', time()));
+    echo "You've last visited this site on: " . $lastVisited;
+    echo "<br> And the time was : " . date('H:i:s') . " (UTC+6)";
+} else
+    echo "Something went wrong!";
+?>
+
+
+
+<!-- 
+    //old code
+
 date_default_timezone_set("Asia/Dacca");
 if (isset($_COOKIE['lastVisited'])) {
     $lastVisited = $_COOKIE['lastVisited'];
@@ -8,6 +30,4 @@ if (isset($_COOKIE['lastVisited'])) {
 
 setcookie('lastVisited', date('d-m-Y', time()));
 echo "Visit: " . $lastVisited;
-echo "<br> Current Time: " . date('H:i:s');
-
-?>
+echo "<br> Current Time: " . date('H:i:s'); -->
